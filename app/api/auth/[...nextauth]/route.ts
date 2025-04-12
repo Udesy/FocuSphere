2aa7ffb8eb7a60a2547fb98a944e640fd21697c3
Uddeshya Prajapati <prajapatiudesy897@gmail.com>
2025-04-12 17:17:21 +0530

    Next auth changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,58 +1,10 @@
-// import NextAuth, { NextAuthOptions } from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-// import GithubProvider from "next-auth/providers/github";
-// import { connectToDB } from "@/utils/database";
-// import User from "@/models/user";
-
-// export const authOptions: NextAuthOptions = {
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.AUTH_GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-//     }),
-//     GithubProvider({
-//       clientId: process.env.AUTH_GITHUB_ID!,
-//       clientSecret: process.env.AUTH_GITHUB_SECRET!,
-//     }),
-//   ],
-//   secret: process.env.AUTH_SECRET!,
-//   callbacks: {
-//     async signIn({ user }) {
-//       await connectToDB();
-
-//       const existingUser = await User.findOne({ email: user.email });
-
-//       if (!existingUser) {
-//         await User.create({
-//           email: user.email,
-//           name: user.name,
-//           username: user.name?.replace(/\s+/g, "").toLowerCase(),
-//           image: user.image,
-//         });
-//       }
-
-//       return true;
-//     },
-//     async session({ session }) {
-//       const user = await User.findOne({ email: session.user?.email });
-//       session.user.id = user._id.toString();
-//       session.user.username = user.username;
-//       session.user.name = user.name;
-//       return session;
-//     },
-//   },
-// };
-
-// const handler = NextAuth(authOptions);
-// export { handler as GET, handler as POST };
-
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import { connectToDB } from "@/utils/database";
 import User from "@/models/user";
 
-export const authOptions: NextAuthOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_CLIENT_ID!,
@@ -83,7 +35,7 @@ export const authOptions: NextAuthOptions = {
         return true;
       } catch (error) {
         console.error("Sign in error:", error);
-        return false; // This will redirect to the error page
+        return false;
       }
     },
     async session({ session }) {
@@ -106,9 +58,8 @@ export const authOptions: NextAuthOptions = {
     },
   },
   pages: {
-    error: "/auth/error", // Custom error page
+    error: "/auth/error",
   },
-};
+});
 
-const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
