522bd55d486f52d1c634fc27b74a436b5ac78fb8
Uddeshya Prajapati <prajapatiudesy897@gmail.com>
2025-04-13 10:57:47 +0530

    Tailiwind font Bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Days_One } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +10,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const dayone = Days_One({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-dayone",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${dayone.variable} antialiased`}
       >
         {children}
       </body>
